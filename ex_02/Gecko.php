bac7a093df58aebd153f593a565529e6339ffb4c
Juliette Gimenez <juliettegimenez@MacBook-de-Juliette-2.local>
2021-07-02 14:22:50 +0200

    ajouter ex_02

--- ex_02/Gecko.php
+++ ex_02/Gecko.php
@@ -1,10 +1,14 @@
 <?php
 class Gecko {
-    public function __construct ($name = NULL) {
+    public $name;
+    public function __construct ($name = null) {
+        $this->name = $name;
          if ($name)
              echo "Hello $name !\n";
         else
             print "Hello !\n";
      }
 }
+$thomas = new Gecko("Thomas");
+$annonymus = new Gecko();
 ?>
